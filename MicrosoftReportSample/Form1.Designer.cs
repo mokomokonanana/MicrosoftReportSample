e9630c3dba2015fe34c96433d460ba4992fcea5b
mokomokonanana <kimama.de.kimagure.chibiakuma+github@gmail.com>
2020-01-14 17:03:08 +0900

    サンプル追加

---
+++ MicrosoftReportSample/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MicrosoftReportSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DataSet1 = new MicrosoftReportSample.DataSet1();
+            this.DataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.DataTable1BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "MicrosoftReportSample.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(1, -1);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 424);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DataSet1
+            // 
+            this.DataSet1.DataSetName = "DataSet1";
+            this.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DataTable1BindingSource
+            // 
+            this.DataTable1BindingSource.DataMember = "DataTable1";
+            this.DataTable1BindingSource.DataSource = this.DataSet1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource DataTable1BindingSource;
+        private DataSet1 DataSet1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MicrosoftReportSample/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicrosoftReportSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DataSet1.DataTable1.AddDataTable1Row("test1", "test1-1", 100);
+            this.DataSet1.DataTable1.AddDataTable1Row("test1", "test1-2", 100);
+            this.DataSet1.DataTable1.AddDataTable1Row("test2", "test2-1", 100);
+            this.DataSet1.DataTable1.AddDataTable1Row("test2", "test2-2", 100);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ReportViewer作成（画面に表示しない）
+            var rv = new Microsoft.Reporting.WinForms.ReportViewer();
+            //  「MicrosoftReportSample.Report1.rdlc」は「プロジェクト名.Reportテンプレートファイル名」
+            rv.LocalReport.ReportEmbeddedResource = "MicrosoftReportSample.Report1.rdlc";
+
+            // データ作成
+            var ds = new DataSet1();
+            ds.DataTable1.AddDataTable1Row("test1", "test1-1", 100);
+            ds.DataTable1.AddDataTable1Row("test1", "test1-2", 100);
+            ds.DataTable1.AddDataTable1Row("test2", "test2-1", 100);
+            ds.DataTable1.AddDataTable1Row("test2", "test2-2", 100);
+
+            // データとReportの紐づけ
+            rv.LocalReport.DataSources.Add(
+                new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", ds.DataTable1.Copy()));
+                // 「DataSet1」はレポート作成時覚えておいた名前
+
+            // Reportにデータ表示
+            rv.RefreshReport();
+
+            // エクセル出力
+            var bytes = rv.LocalReport.Render("EXCELOPENXML");
+            using (var fs = new System.IO.FileStream("test.xlsx", System.IO.FileMode.Create)) // exeと同階層にファイルを出力
+            {
+                fs.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
